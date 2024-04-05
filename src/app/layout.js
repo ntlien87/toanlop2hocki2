ee6b4bc98c2579dcf7af0bbf212a9ed7572b8f84
ntlien87 <ntlien291187@gmail.com>
2024-04-05 08:56:14 +0700

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,12 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Cung Nhau Hoc Toan",
-  description:
-    "Vài đề ôn tập toán lớp 2 học kì 2 cho các bé làm trực tiếp trên máy tính hoặc điện thoại",
-};
+  title: 'Cung Nhau Hoc Toan',
+  description: 'Đề thi toán lớp 2 kì 2',
+}
 
 export default function RootLayout({ children }) {
   return (
@@ -20,5 +19,5 @@ export default function RootLayout({ children }) {
         </div>
       </body>
     </html>
-  );
+  )
 }
